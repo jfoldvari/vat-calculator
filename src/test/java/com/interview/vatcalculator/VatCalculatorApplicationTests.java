e4fc948c114331aaa8004117b38bb536ddbe2b05
jfoldvari <130476503+jfoldvari@users.noreply.github.com>
2023-04-11 17:10:29 +0200

    Initial project structure

---
+++ src/test/java/com/interview/vatcalculator/VatCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.vatcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VatCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
